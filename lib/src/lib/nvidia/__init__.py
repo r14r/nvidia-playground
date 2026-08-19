ea1ecd0e2bd62efa4b4887f39fb7f309a7891700
Ralph Göstenmeier <r14r@via-internet.de>
2026-08-19 11:02:16 +0200

    Add nvidia-lib

---
+++ lib/src/lib/nvidia/__init__.py
@@ -0,0 +1,3 @@
+from .client import NVIDIA
+from .exceptions import NVIDIAError,ModelCatalogError,ModelNotFoundError,AmbiguousModelError,ModelCredentialError
+__all__=["NVIDIA","NVIDIAError","ModelCatalogError","ModelNotFoundError","AmbiguousModelError","ModelCredentialError"]
